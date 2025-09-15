ed3e85816590ef857e0661065dbff8348b085664
Dmytro <d.ponomarev267@gmail.com>
2025-09-15 22:01:48 +0300

    Add greetUser function to App.java

--- MyApp/app/src/main/java/myapp/App.java
+++ MyApp/app/src/main/java/myapp/App.java
@@ -4,11 +4,11 @@
 package myapp;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println("Hello World!");
+        greetUser("User");
+    }
+    public String greetUser(String name) {
+        return "Hello " + name + "!";
     }
 }
